0806b043a259d74c22d2d7f664af8d26ec614af8
Hamilton Codes <reggieh2012@gmail.com>
2021-01-05 09:53:30 -0600

    Update index.js
    delete test txt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const keys = require('./keys');
 
 // Express App Setup
-// Just to see if this worked --- Delete ME
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
