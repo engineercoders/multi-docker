51666a85c003b43f9e745f424a8ddbfd4f5a279c
Reggie Hamilton <reggieh2012@gmail.com>
2021-01-05 09:53:52 -0600

    test

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const keys = require('./keys');
 
 // Express App Setup
+// Just to see if this worked --- Delete ME
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
